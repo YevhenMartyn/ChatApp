9227e3440036911775be9fb1c3d1f9ce766c5772
Yevhen Martyn <martyn.yevhen@gmail.com>
2026-08-24 21:41:29 +0200

    apiGateway intemediate coomit

---
+++ backend/ChatApp.ApiGateway/src/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("GatewayCorsPolicy", policy =>
+    {
+        policy.WithOrigins(builder.Configuration["FrontendUrl"] ?? "http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("GatewayCorsPolicy");
+
+app.MapReverseProxy();
+
+app.Run();
